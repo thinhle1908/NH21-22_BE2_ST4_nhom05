10427767067c77862b70352e78075ef8fa7552cd
thinhle1908 <20211tt0718@mail.tdc.edu.vn>
2022-04-26 16:14:10 +0700

    thinhcommit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-       
+        $this->call([
+            UserSeeder::class
+        ]);
+        $this->call([
+            Cart::class
+        ]);
+        $this->call([
+            UserSeeder::class
+        ]);
+        $this->call([
+            UserSeeder::class
+        ]);
+        $this->call([
+            UserSeeder::class
+        ]);
+        $this->call([
+            UserSeeder::class
+        ]);
     }
 }

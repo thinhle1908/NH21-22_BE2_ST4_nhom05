755dc55832f9058592a06f033ed1a72cc28fdc16
theGIFTofGOD <JonathanBui.dev@gmail.com>
2022-05-21 11:41:44 +0700

    Xay dung trang login
    Xay dung trang login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,10 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-10 h-1 fill-current text-gray-500" />
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
             </a>
         </x-slot>
-
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
 
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
@@ -15,11 +12,13 @@
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
+      
+
             <!-- Email Address -->
-            <div>
+            <div class="mt-4">
                 <x-label for="email" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
             </div>
 
             <!-- Password -->
@@ -29,28 +28,64 @@
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
                                 name="password"
-                                required autocomplete="current-password" />
+                                required autocomplete="new-password" />
             </div>
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+           
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                    {{ __('Dont have account?') }}
+                </a>
 
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
+                <x-button class="ml-4">
+                    {{ __('Login') }}
                 </x-button>
             </div>
         </form>
     </x-auth-card>
+</x-guest-layout> --}}
+<x-guest-layout>
+
+    <div class="container">
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        
+        <br>
+        <form class="row col-md-6 col-md-offset-3 margin-right" method="POST" action="{{ route('login') }}">
+            @csrf
+            <!-- Logo -->
+            <div class="logo">
+                <a href="index"><img  class= " center-block"src="images/home/logo.png" alt="" /></a>
+            </div>
+            <h2>Login</h2>
+            
+            <!-- Email Address -->
+            <div class="form-group">
+                <x-label for="email" :value="__('Email')" />
+
+                <x-input id="email" class="form-control" type="email" name="email" :value="old('email')" required />
+            </div>
+
+            <!-- Password -->
+            <div class="form-group">
+                <x-label for="password" :value="__('Password')" />
+
+                <x-input id="password" class="form-control" type="password" name="password" required
+                    autocomplete="new-password" />
+            </div>
+
+           
+
+            <div class="form-group " style="padding-left:30%">
+                <a class="btn btn-warning "  href="{{ route('register') }}">
+                    {{ __('Dont have account?') }}
+                </a>
+
+                <x-button class="btn btn-light">
+                    {{ __('Login') }}
+                </x-button>
+            </div>
+        </form>
+    </div>
 </x-guest-layout>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -23,13 +23,9 @@
 				</div>
 				<div class="col-sm-4">
 					<div class="signup-form"><!--sign up form-->
-						<h2>New User Signup!</h2>
-						<form action="#">
-							<input type="text" placeholder="Name"/>
-							<input type="email" placeholder="Email Address"/>
-							<input type="password" placeholder="Password"/>
-							<button type="submit" class="btn btn-default">Signup</button>
-						</form>
+					<a href="/register">New User Signup!</a>
+						
+					
 					</div><!--/sign up form-->
 				</div>
 			</div>

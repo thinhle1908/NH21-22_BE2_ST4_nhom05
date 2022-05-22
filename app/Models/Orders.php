bc544e8d6d4b62431b8b0001a2c877aa3e8d0a65
DuongVanQuang <20211tt2540@mail.tdc.edu.vn>
2022-05-22 12:26:29 +0700

    quang commit

---
+++ app/Models/Orders.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Orders extends Model
+{
+    use HasFactory;
+    protected $table = 'orders';
+    protected $fillable = [
+        'order_name',
+        'order_address',
+        'order_phone',
+        'order_email',
+        'order_notes',
+    ];
+}
---
+++ database/migrations/2022_05_22_041348_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_name');
+            $table->string('order_address');
+            $table->string('order_phone');
+            $table->string('order_email');
+            $table->string('order_notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}

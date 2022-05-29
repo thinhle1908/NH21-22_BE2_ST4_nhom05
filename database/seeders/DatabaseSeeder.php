bf79b648da34ca621b9aa1ca5f927f85bf767e78
DuongVanQuang <20211tt2540@mail.tdc.edu.vn>
2022-05-29 09:43:57 +0700

    quang commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,5 +22,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ProductsSeeder::class
         ]);
+        $this->call([
+            VouchersSeeder::class
+        ]);
     }
 }
